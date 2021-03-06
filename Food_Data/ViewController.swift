a24e577ac956e3d1383ca960da6405e4073dcd6e
Jonathan Poon <poonj@umich.edu>
2021-03-05 19:08:10 -0500

    Initial Commit

---
+++ Food_Data/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food_Data
+//
+//  Created by Jonathan Poon on 3/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
